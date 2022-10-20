56e1a4d5880084bbbae9c0a0e06cc8d918a32f3d
Oleg Denisenko <denisenko.oleg.fortech@gmail.com>
2022-10-20 12:57:32 +0300

    Basic UI setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,8 +5,16 @@ import Image from 'next/image'
 
 const Home: NextPage = () => {
   return (
-    <div className="font-bold">
-     Hello world
+    <div className="h-full w-full flex flex-col justify-center items-center text-center">
+     <h1 className='text-2xl font-bold mb-6'>Which one is Roundest???</h1>
+
+     <div className='grid grid-cols-[minmax(100px,1fr)_auto_minmax(100px,1fr)] gap-x-10 w-2/3 max-w-4xl border-2 border-color-yellow-800 p-10'>
+      <div className='h-96 bg-red-200 flex items-center justify-center'>CONTENT</div>
+      
+      <div className='self-center text-xl'>Vs</div>
+      
+      <div className='h-96 bg-blue-500 flex items-center justify-center'>CONTENT</div>
+     </div>
     </div>
   )
 }
